6b16a718238390f4d7206ca0655cc3c84bfdbf0e
junhyeoksin <h-kun@naver.com>
2024-01-10 11:33:53 +0900

    Update

--- keystorm-Discovery/src/main/java/keystormDiscovery/Application.java
+++ keystorm-Discovery/src/main/java/keystormDiscovery/Application.java
@@ -2,8 +2,10 @@ package keystormDiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer    //  Add
 public class Application {
 
 	public static void main(String[] args) {
